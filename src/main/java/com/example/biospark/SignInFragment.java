6006299193c7a2092ffe0c2d1242230f75688b32
rezwanakib <rezwanakib@gmail.com>
2020-10-26 19:17:55 +0600

    sign in page complete

--- src/main/java/com/example/biospark/SignInFragment.java
+++ src/main/java/com/example/biospark/SignInFragment.java
@@ -2,18 +2,17 @@ package com.example.biospark;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
-import android.widget.TextView;
 
-
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SignInFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
 public class SignInFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
@@ -47,9 +46,6 @@ public class SignInFragment extends Fragment {
         return fragment;
     }
 
-    private TextView  dontHaveAnId;
-    private FrameLayout parentFrameLayout;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -63,28 +59,6 @@ public class SignInFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_sign_in, container, false);
-        dontHaveAnId = view.findViewById(R.id.tv_dont_have_an_id);
-        parentFrameLayout = getActivity().findViewById(R.id.register_framelayout);
-        return view;
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        dontHaveAnId.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                setFragment(new SignUpFragment());
-            }
-        });
-    }
-
-    private void setFragment(Fragment fragment) {
-        FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.setCustomAnimations(R.anim.slide_from_right,R.anim.slideout_from_left);
-        fragmentTransaction.replace(parentFrameLayout.getId(),fragment);
-        fragmentTransaction.commit();
+        return inflater.inflate(R.layout.fragment_sign_in, container, false);
     }
 }
